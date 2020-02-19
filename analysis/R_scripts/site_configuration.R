59e0c1cdd48f1c7c8da40ce56baf0114cb6ba54f
Kevin See <Kevin.See@merck.com>
2020-02-19 08:37:35 -0800

    added some testing code at the bottom of site_configuration.R

--- analysis/R_scripts/site_configuration.R
+++ analysis/R_scripts/site_configuration.R
@@ -94,3 +94,104 @@ parent_child %>%
 
 parent_child %>%
   filter(grepl('539\\.', RKM))
+
+parent_child %>%
+  filter(nodeOrder > 1) %>%
+  select(ChildNode) %>%
+  mutate_at(vars(ChildNode),
+            list(~ paste0(., "_p ~ dbeta(1,1)"))) %>%
+  write_delim(path = '/Users/seek/Desktop/PRO_sites.txt',
+              delim = '\n')
+
+
+parent_child %>%
+  filter(nodeOrder == 2) %>%
+  select(ChildNode) %>%
+  mutate_at(vars(ChildNode),
+            list(~ str_remove(., "B0$")))
+
+library(DABOM)
+setBranchNums(parent_child)
+valid_paths = getValidPaths(parent_child,
+                            root_site = "PRO")
+node_order = createNodeOrder(valid_paths,
+                             configuration,
+                             site_df,
+                             step_num = 2) %>%
+  select(-Group) %>%
+  left_join(tibble(BranchNum = 1:8,
+                   Group = c(rep("Downstream", 5),
+                             'Status',
+                             'Toppenish',
+                             'Sunnyside')))
+node_order %>%
+  filter(NodeOrder == 2)
+
+createNodeList(node_order)
+
+
+
+branch_nums  = setBranchNums(parent_child) %>%
+  unlist() %>%
+  enframe(name = 'var',
+          value = "n_branch") %>%
+  mutate(SiteID = str_remove(var, 'n_pops_')) %>%
+  select(SiteID, n_branch)
+
+
+site = "ROZ"
+
+n_branch_site = branch_nums %>%
+  filter(SiteID == site) %>%
+  pull(n_branch)
+
+
+node_order %>%
+  filter(grepl(site, Path)) %>%
+  select(NodeSite) %>%
+  distinct() %>%
+  slice(1:n_branch_site) %>%
+  filter(NodeSite != site) %>%
+  mutate(nm = paste0('past_', NodeSite)) %>%
+  pull(nm) %>%
+  c(., paste0(site, "_bb"))
+
+parent_child %>%
+  mutate(ParentSite = str_remove(str_remove(ParentNode, "B0$"), "A0$"),
+         ChildSite = str_remove(str_remove(ChildNode, "B0$"), "A0$"))
+
+parent_child %>%
+  mutate_at(vars(ParentNode, ChildNode),
+            list(~ str_remove(str_remove(., "B0$"), "A0$"))) %>%
+  rename(ParentSite = ParentNode,
+         ChildSite = ChildNode) %>%
+  filter(ParentSite != ChildSite) %>%
+  group_by(ParentSite) %>%
+  summarise(n_childs = n_distinct(ChildSite),
+            child_sites = list(ChildSite)) %>%
+  # filter(n_childs > 1) %>%
+  mutate(vec_nm = map(child_sites,
+                      .f = function(x) {
+                        paste0('past_', x)
+                      })) %>%
+  mutate(vec_nm = map2(vec_nm,
+                       ParentSite,
+                       .f = function(x, y) {
+                         if(length(x) > 1) {
+                           c(x, paste0(y, "_bb"))
+                         } else {
+                           x
+                         }
+                       })) %>%
+  mutate(param_nm = paste0("p_pop_", ParentSite)) %>%
+  unnest(vec_nm) %>%
+  group_by(param_nm) %>%
+  mutate(vec_pos = 1:n()) %>%
+  ungroup() %>%
+  mutate(param_nm = if_else(n_childs == 1,
+                            str_replace(vec_nm, 'past', 'phi'),
+                            param_nm)) %>%
+  mutate(param_nm = if_else(n_childs > 1,
+                            paste0(param_nm, "[", vec_pos, "]"),
+                            param_nm)) %>%
+  select(param_nm, vec_nm)
