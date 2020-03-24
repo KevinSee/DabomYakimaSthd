f41d5108b8d081d519976cbd35af901f07faf45b
Kevin See <Kevin.See@merck.com>
2020-03-24 16:06:24 -0700

    read in and saved biological data for 2012-2014

--- analysis/R_scripts/02_bio_data.R
+++ analysis/R_scripts/02_bio_data.R
@@ -1,7 +1,7 @@
 # Author: Kevin See
 # Purpose: create tag lists to feed to PTAGIS query
 # Created: 2/19/20
-# Last Modified: 2/27/20
+# Last Modified: 3/24/20
 # Notes:
 
 #-----------------------------------------------------------------
@@ -10,6 +10,7 @@
 library(tidyverse)
 library(readxl)
 library(janitor)
+library(magrittr)
 
 #-----------------------------------------------------------------
 # read in biological data from trap
@@ -43,3 +44,77 @@ write_delim(tags,
 # save biological data for later
 write_rds(bio_df,
           path = 'analysis/data/derived_data/Bio_2018_19.rds')
+
+#-----------------------------------------------------------------
+# for older years
+bio_df = 2012:2014 %>%
+  as.list %>%
+  rlang::set_names() %>%
+  map_df(.id = 'Year',
+         .f = function(yr) {
+
+           df = read_excel(paste0('analysis/data/raw_data/YakimaNation/Prosser_steelhead_', yr, '.xlsx'))
+
+           if("WDFW Age" %in% names(df)) {
+             df %<>%
+               rename(age = `WDFW Age`)
+           }
+
+           if("ProsserDate...8" %in% names(df)) {
+             df %<>%
+               rename(ProsserDate = `ProsserDate...8`)
+           }
+
+           df %>%
+             mutate(TagID = if_else(!is.na(pittag),
+                                    pittag,
+                                    jvpittag)) %>%
+             rename(SppCode = SpeciesCode) %>%
+             mutate(alt_tag = if_else(!is.na(pittag) & !is.na(jvpittag),
+                                      jvpittag,
+                                      as.character(NA))) %>%
+             select(PassDate = ProsserDate,
+                    TagID,
+                    alt_tag,
+                    SppCode,
+                    Status = status,
+                    age,
+                    sex,
+                    forklgth,
+                    mehlgth,
+                    weight,
+                    AdiposeClip,
+                    comments) %>%
+             clean_names(case = 'upper_camel') %>%
+             rename(TagID = TagId)
+         })
+
+tabyl(bio_df, Year)
+xtabs(~ Year + is.na(AltTag), bio_df)
+
+bio_df %>%
+  filter(Year != 2012,
+         !is.na(AltTag))
+
+# pull out PIT tag numbers
+tags = bio_df %>%
+  split(list(.$Year)) %>%
+  map(.f = function(x) {
+    x %>%
+      select(TagID) %>%
+      bind_rows(x %>%
+                  filter(!is.na(AltTag)) %>%
+                  select(TagID = AltTag))
+  })
+
+# save tags to upload to PTAGIS
+for(yr in names(tags)) {
+  write_delim(tags[[yr]],
+              path = paste0('analysis/data/raw_data/tag_lists/Tags_', yr, '.txt'),
+              delim = '\n',
+              col_names = F)
+}
+
+# save biological data for later
+write_rds(bio_df,
+          path = 'analysis/data/derived_data/Bio_2012_14.rds')
